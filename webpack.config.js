f6fd6771df45875d1dfe929b2f864579cf74fbce
Victor Koech <vickoch20@gmail.com>
2024-07-20 15:35:47 +0300

    Add publicPath to my repo

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         clean: true,
+        publicPath: 'portfolio-homepage',
     },
     module: {
         rules: [
