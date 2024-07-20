55473b84baa11042d288b98f392c92d79c402f33
Victor Koech <vickoch20@gmail.com>
2024-07-20 14:09:53 +0300

    Update webpack.config.js to use the lastest syntax

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,6 @@ module.exports = {
                 ],
             },
             {
-                test: /\.css$/,
-                use: [
-                  MiniCssExtractPlugin.loader,
-                  'css-loader'
-                ],
-              },
-            {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
